5511ade31f73880e594650f0cbfed910fd4c3841
seungsuYU <164775682+seungsuYU@users.noreply.github.com>
2024-10-12 15:20:29 +0900

    db연결

--- src/main/java/com/sparta/spring_prepare/Repository/PostListRepository.java
+++ src/main/java/com/sparta/spring_prepare/Repository/PostListRepository.java
@@ -2,6 +2,7 @@ package com.sparta.spring_prepare.Repository;
 
 import com.sparta.spring_prepare.Entity.PostList;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -9,4 +10,8 @@ import java.util.List;
 @Repository
 public interface PostListRepository extends JpaRepository<PostList, Long> {
     List<PostList> findByTitle(String title);
+
+    private final JdbcTemplate jdbctemplate;
+
+    }
 }
